9064f9f17edd01c968f44a5917ec4794c417d288
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2025-04-02 23:52:40 +0300

    dtos created

---
+++ src/main/java/org/sau/devopsv2/dto/EmployeeDTO.java
@@ -0,0 +1,32 @@
+package org.sau.devopsv2.dto;
+
+public class EmployeeDTO {
+    private Long id;
+    private String name;
+    private String department;
+
+    // Getter ve Setter metotları
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
