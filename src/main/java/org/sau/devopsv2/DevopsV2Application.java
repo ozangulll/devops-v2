a6aef93ea2b02f2a2d2960e6c495d7020ff123b2
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2025-04-02 23:09:47 +0300

    initial commit

---
+++ src/main/java/org/sau/devopsv2/DevopsV2Application.java
@@ -0,0 +1,13 @@
+package org.sau.devopsv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsV2Application.class, args);
+    }
+
+}
