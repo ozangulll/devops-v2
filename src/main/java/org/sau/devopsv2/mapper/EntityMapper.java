c25ebcc5fabb7a3c3a1d75edddc9e0a23145f0fe
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2025-04-03 12:25:01 +0300

    mapper updated.

--- src/main/java/org/sau/devopsv2/mapper/EntityMapper.java
+++ src/main/java/org/sau/devopsv2/mapper/EntityMapper.java
@@ -7,6 +7,7 @@ import org.sau.devopsv2.entity.Task;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
@@ -18,16 +19,26 @@ public class EntityMapper {
         dto.setId(employee.getId());
         dto.setName(employee.getName());
         dto.setDepartment(employee.getDepartment());
+        dto.setTaskIds(employee.getTasks().stream().map(Task::getId).collect(Collectors.toSet()));
         return dto;
     }
 
-    public Employee convertToEmployeeEntity(EmployeeDTO dto) {
+    public Employee convertToEmployeeEntity(EmployeeDTO dto, Set<Task> tasks) {
         Employee employee = new Employee();
         employee.setId(dto.getId());
         employee.setName(dto.getName());
         employee.setDepartment(dto.getDepartment());
+        Set<Task> taskSet = dto.getTaskIds().stream()
+                .map(taskId -> {
+                    Task task = new Task();
+                    task.setId(taskId);
+                    return task;
+                })
+                .collect(Collectors.toSet());
+        employee.setTasks(taskSet);
         return employee;
     }
+
 
     public List<EmployeeDTO> convertToEmployeeDTOList(List<Employee> employees) {
         return employees.stream().map(this::convertToEmployeeDTO).collect(Collectors.toList());
@@ -39,14 +50,16 @@ public class EntityMapper {
         dto.setId(task.getId());
         dto.setName(task.getName());
         dto.setDescription(task.getDescription());
+        dto.setEmployeeIds(task.getEmployees().stream().map(Employee::getId).collect(Collectors.toSet()));
         return dto;
     }
 
-    public Task convertToTaskEntity(TaskDTO dto) {
+    public Task convertToTaskEntity(TaskDTO dto, Set<Employee> employees) {
         Task task = new Task();
         task.setId(dto.getId());
         task.setName(dto.getName());
         task.setDescription(dto.getDescription());
+        task.setEmployees(employees);
         return task;
     }
 
