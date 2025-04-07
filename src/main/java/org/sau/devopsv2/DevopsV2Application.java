e73920ff152356cf32ef7c1d10f31a57ed8093b7
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2025-04-07 14:06:58 +0300

    annotation

--- src/main/java/org/sau/devopsv2/DevopsV2Application.java
+++ src/main/java/org/sau/devopsv2/DevopsV2Application.java
@@ -2,8 +2,11 @@ package org.sau.devopsv2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("org.sau.devopsv2.repository")  // Ensure this points to your repository package
+
 public class DevopsV2Application {
 
     public static void main(String[] args) {
